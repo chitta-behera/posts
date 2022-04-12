1b5a818f6c34bc4513341c3b6f1754d6cba97c54
behera_cr <behera.cr@gmail.com>
2022-04-12 08:59:20 +0000

    initial commit

---
+++ posts/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);  
+});
+
+app.post('/posts', (req, res) => {
+	console.log('blog post create ..');
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id, title 
+  };
+
+/*await axios.post('http://comments-srv:4001/comments/check', {
+  data:{
+    'name': 'from posts'
+  }
+});
+*/
+	
+
+  res.status(201).send(posts[id]);
+});
+
+
+const server = app.listen(4002, () => {
+  console.log('v3:google cloud kubernetes...');
+  console.log('Listing on 4002 port...');
+})
